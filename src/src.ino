c8403c6cf6550847c1214572a948ee6d7c2c6b59
Ahmed Aladdin <aamieeu@gmail.com>
2024-05-11 15:36:50 +0300

    prepar the src.ino

--- src/src.ino
+++ src/src.ino
@@ -1,28 +1,57 @@
 #include "headers/DishWasher.h"
 #include "headers/Constants.h"
 #include "headers/Crawler.h"
-
+#include "headers/Arm.h"
 DishWasher disher(fanPin,pumPin);
 Crawler crawler(leftForward,leftBackward,speedLeft,rightForward,rightBackward,speedRight);
+Arm arm(gripPin,rotationPin,upPin,downPin);
 
-void setup(){
-  // disher.setup();
-  // crawler.setup();
-  pinMode(8, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(6, OUTPUT);
+int incomingByte = 0;
+
+bool forwardsPressed = false;
+bool backwardsPressed = false;
+bool rightPressed = false;
+bool leftPressed = false;
+
+void setup() {
+  crawler.setup();
+  disher.setup();
+  Serial.begin(9600);
+  arm.setup();
 }
 
-void loop(){
+void loop() {
+  if (Serial.available() > 0) {
+    
+    char c = (char)Serial.read();
+    Serial.println(c);
+      digitalWrite(13, HIGH);
+    if(c == 'U'){
+      crawler.forward();
+    }else if(c == 'D'){
+      crawler.backward();
+    }else if(c == 'R'){
+      crawler.right();
+    }else if(c == 'L'){
+      crawler.left();
+    }else{
+      crawler.stop();
+      digitalWrite(13, LOW);
+    }
 
-  digitalWrite(8, HIGH);
-  digitalWrite(11, LOW);
-  analogWrite(6, 255);
-  //digitalWrite(rightForward,HIGH);
-  // digitalWrite(5, 2);
-  // analogWrite(5, 255);
-  // disher.startFan();
-  // disher.startPumb();
+    if(c == 'z'){
+      arm.up();
+    }else if(c == 'x'){
+      arm.down();
+    }else arm.stopLifting();
 
-  //crawler.backward();
+    if(c == 'r'){
+      arm.rotateDown();
+    }else if(c == 't'){
+      arm.rotateUp();
+    }
+
+
+  }
+  delay(100);
 }
